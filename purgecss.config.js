968004856e67cd50e111720bf262f8c4f2e6781f
Mayank Bisht <bishtmayank293@gmail.com>
2026-04-28 07:46:19 +0530

    remove unused purgecss config

--- purgecss.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  content: ['./client/src/**/*.{js,jsx,ts,tsx}'],
-  css: ['./client/src/styles/pages.css'],
-  output: './client/src/styles/clean-pages.css',
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  safelist: {
-    standard: [/^-/],
-  },
-}
